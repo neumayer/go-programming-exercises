3992858270abff19e77bf8e1e58d6b1d3631563c
Robert Neumayer <neumayer@comoyo.com>
2016-12-03 19:45:23 +0100

    Make more generic and shorter

--- arrays/bubblesort.go
+++ arrays/bubblesort.go
@@ -1,25 +1,18 @@
 package arrays
 
-import "github.com/neumayer/go-programming-exercises/utils"
-
 type bubblesort struct {
 }
 
 // Sort the given array.
 func (s bubblesort) Sort(array []int) {
-	for i := 0; i < len(array); i++ {
-		swapPerformed := false
+	for j := 0; j < len(array); j++ {
 		for i := 0; i < len(array); i++ {
-			if i+1 == len(array) {
-				break
+			if i >= len(array)-1 {
+				continue
 			}
 			if array[i] > array[i+1] {
-				arrayutils.Swap(array, i, i+1)
-				swapPerformed = true
+				array[i], array[i+1] = array[i+1], array[i]
 			}
-		}
-		if !swapPerformed {
-			break
 		}
 	}
 }
