029f87baa5752a71f0abbcc54e0f338a79b74442
Robert Neumayer <neumayer@comoyo.com>
2017-01-20 11:15:51 +0100

    Add inline version of string reversal

--- strings/reverse.go
+++ strings/reverse.go
@@ -26,3 +26,32 @@ func reverse(value []rune) string {
 	}
 	return string(resultSlice)
 }
+
+func reverseInline(value []rune) {
+	reverseString(value)
+
+	var wordBeginIndex = 0
+	var wordEndIndex = 0
+	for readIndex := 0; readIndex < len(value); readIndex++ {
+		if value[readIndex] == ' ' {
+			wordEndIndex = readIndex - 1
+			reverseSubString(value, wordBeginIndex, wordEndIndex)
+			wordBeginIndex = readIndex + 1
+		}
+		if readIndex == len(value)-1 {
+			wordEndIndex = readIndex
+			reverseSubString(value, wordBeginIndex, wordEndIndex)
+			wordBeginIndex = readIndex
+		}
+	}
+}
+
+func reverseString(value []rune) {
+	reverseSubString(value, 0, len(value)-1)
+}
+
+func reverseSubString(value []rune, begin int, end int) {
+	for ; begin < end; begin, end = begin+1, end-1 {
+		value[begin], value[end] = value[end], value[begin]
+	}
+}
--- strings/reverse_test.go
+++ strings/reverse_test.go
@@ -1,6 +1,7 @@
 package strings
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -16,7 +17,45 @@ func TestRevert(t *testing.T) {
 		res := reverse(tt.value)
 
 		if res != tt.expected {
-			t.Errorf("Revert (%d): expected %d, actual %d", tt.value, tt.expected, res)
+			t.Errorf("Revert (%d): expected %d, actual %d", string(tt.value), string(tt.expected), string(res))
+		}
+	}
+}
+
+func TestRevertInline(t *testing.T) {
+	var removeTests = []struct {
+		value    []rune
+		expected []rune
+	}{
+		{[]rune("first second third"), []rune("third second first")},
+		{[]rune("first second"), []rune("second first")},
+	}
+	for _, tt := range removeTests {
+		reversedValue := make([]rune, len(tt.value))
+		copy(reversedValue, tt.value)
+		reverseInline(reversedValue)
+
+		if !reflect.DeepEqual(reversedValue, tt.expected) {
+			t.Errorf("Revert (%d): expected %d, actual %d", string(tt.value), string(tt.expected), string(reversedValue))
+		}
+	}
+}
+
+func TestRevertString(t *testing.T) {
+
+	var removeTests = []struct {
+		value    []rune
+		expected []rune
+	}{
+		{[]rune("abc"), []rune("cba")},
+	}
+	for _, tt := range removeTests {
+		reversedValue := make([]rune, len(tt.value))
+		copy(reversedValue, tt.value)
+		reverseString(reversedValue)
+
+		if !reflect.DeepEqual(reversedValue, tt.expected) {
+			t.Errorf("Revert (%d): expected %d, actual %d", string(tt.value), string(tt.expected), string(reversedValue))
 		}
 	}
 }
