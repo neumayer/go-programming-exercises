b68e1833745e98067566c9416b847d5bd6f82dac
Robert Neumayer <neumayer@comoyo.com>
2017-02-23 20:57:00 +0100

    Simplify array reverse function

--- arrays/reverse.go
+++ arrays/reverse.go
@@ -1,13 +1,8 @@
 package arrays
 
-import (
-	"github.com/neumayer/go-programming-exercises/utils"
-)
-
-// Reverse reverses an array.
-func Reverse(array []int) {
-	lastIndex := len(array) - 1
-	for i := 0; i < len(array)/2; i++ {
-		arrayutils.Swap(array, i, lastIndex-i)
+// Reverse reverses an int slice.
+func Reverse(slice []int) {
+	for i, j := 0, len(slice)-1; i < len(slice)/2; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
 	}
 }
